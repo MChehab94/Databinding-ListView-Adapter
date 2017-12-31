f799201b60329f7483e96eab2dcb81c3c243bc3b
Mohamad <lemirmohammad.shehab@lau.edu>
2017-12-31 22:01:28 +0200

    Added imageURL variable

--- kotlinapp/src/main/java/mchehab/com/kotlinapp/Person.kt
+++ kotlinapp/src/main/java/mchehab/com/kotlinapp/Person.kt
@@ -3,4 +3,4 @@ package mchehab.com.kotlinapp
 /**
  * Created by muhammadchehab on 12/31/17.
  */
-data class Person(val firstName: String, val lastName: String, val age: Int)
+data class Person(val firstName: String, val lastName: String, val imageURL: String, val age: Int)
